c845312ca5df875befe3c9a7384c1e2bb528a0e8
Roberto <Roberto@ROBBER>
2025-06-27 13:21:21 -0600

    Problema de formularios arreglado

--- Prototipo_MarZel/obj/Release/net8.0-windows/Prototipo_MarZel.AssemblyInfo.cs
+++ Prototipo_MarZel/obj/Release/net8.0-windows/Prototipo_MarZel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4b3a51ce58f09b24cf450420269a18fa8077044")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
