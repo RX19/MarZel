f4b3a51ce58f09b24cf450420269a18fa8077044
Marcos Maradiaga <mamaradiagaz@gmail.com>
2025-06-26 23:21:56 -0600

    Creacion de formulacio de Ventas

---
+++ Prototipo_MarZel/Formularios/REGISTRAR_VENTAS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo_MarZel.Formularios
+{
+    public partial class REGISTRAR_VENTAS : MaterialSkin.Controls.MaterialForm
+    {
+        public REGISTRAR_VENTAS()
+        {
+            InitializeComponent();
+        }
+
+        private void REGISTRAR_VENTAS_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
