117a1bf6bfdb4555c3c0c3c21382b0d4419dd47d
rusbe <rusbe@LEGION5I>
2025-07-22 19:55:34 -0600

    Form Pipeline status test

---
+++ Prototipo_MarZel/Formularios/Form_Test_Pipeline.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Prototipo_MarZel.Formularios
+{
+    partial class Form_Test_Pipeline
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form_Test_Pipeline
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "Form_Test_Pipeline";
+            Text = "Form_Test_Pipeline";
+            Load += Form_Test_Pipeline_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
