784d51df5b1a53f650c0963fddbfe706545197ed
GerardoMayorga <162228640+GerardoMayorga@users.noreply.github.com>
2025-07-23 19:06:38 -0600

    cambios para pull

--- Prototipo_MarZel/obj/Debug/net8.0-windows/Prototipo_MarZel.AssemblyInfo.cs
+++ Prototipo_MarZel/obj/Debug/net8.0-windows/Prototipo_MarZel.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3754e94bab6d9034b70258d68dbef01d88a87406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cc52457f53e4d5c0dbff8205a8b5645daa98c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prototipo_MarZel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
